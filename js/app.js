8981d40742f8b73fbadb230d73e2ffd2632627c2
alch3m1st <bryan.ale.3240@gmail.com>
2024-07-16 21:31:13 -0400

    upgrading the javascript code and the html + new style: - So I solve my problem that nothing shows in the page - Now when you put the city the temperature, humidity, cloud cover shows in the page with his respective icon - I change the name of some ids in the index.html - The weather don't show to the user until this one sent the city input

--- js/app.js
+++ js/app.js
@@ -35,28 +35,73 @@
 
 // https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}
 
+// Solve the issue of nth display in the web console
+// Solve the issue of nth display in the web console
+// Solve the issue of nth display in the web console
+// Solve the issue of nth display in the web console
+
+
+// Try to run the code on RunJs
+// Try to run the code on RunJs
+// Try to run the code on RunJs
+// Try to run the code on RunJs
+// Try to run the code on RunJs
+// Try to run the code on RunJs
+// Try to run the code on RunJs
+
+const weatherForm = document.querySelector('#weatherForm');
+const cityInput = document.querySelector('#cityInput');
+
+weatherForm.addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent form submission default behavior
+    temperature(); // Call the temperature function when the form is submitted
+});
+
 async function temperature() {
   try {
-    const city = document.querySelector('#cityInput');
-    const cityValue = city.value.trim();
+    const city = cityInput.value.trim();
+    // const city = 'Belleville'
+    // const cityValue = city.value.trim();
     const apiKey = '6b055f484fb6fc59adad6b10963fef35'
-    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${cityValue}&appid=${apiKey}`)
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}`)
 
-    console.log('Response received:', response);
+    // console.log('Response received:', response);
 
     if (!response.ok) {
         throw new Error('Network response was not ok');
     }
 
     const data = await response.json();
+    // Add weather icon URL
+    const weatherIcon = data.list[0].weather[0].icon;
+    const iconUrl = `http://openweathermap.org/img/wn/${weatherIcon}@2x.png`;
+    
+   // Convert temperature from Kelvin to Fahrenheit
+    const kelvin = data.list[0].main.temp_max;
+    const num = (kelvin - 273.15) * 9/5 + 32;
+    const farenheit = Math.trunc(num);
+
+    document.getElementById('city').textContent = city;
+    document.getElementById('temperatureFarenheit').textContent = `Temperature: ${farenheit} F`;
+    document.getElementById('cloudCover').textContent = `Cloud Cover: ${data.list[0].weather[0].description}`;
+    document.getElementById('humidity').textContent = `Humidity: ${data.list[0].main.humidity}%`;
+    document.getElementById('weatherIcon').src = iconUrl;
+    // document.getElementById('weatherEmoji').src = iconUrl;
+    
+    
+    document.querySelector('.weatherCard').style.display = 'block';
 
     // const firstItem = data.list[0];
     // const f = ''
 
-    console.log(data)
+    // console.log(data)
   } catch (error) {
     console.error('Error fetching data:', error);
   } // This is the base of the code to start, I need to get the values of the json code
 };
+
+async function weatherToCelsius () {
+  
+}
 
 // document.getElementById('searchCityBtn').addEventListener('click', temperature)
